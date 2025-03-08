16ff4a907847fb8e6cf230f34778021b60c83cae
neeravdubal <nsdubal@hotmail.com>
2025-03-08 10:05:56 +0100

    Indentation updated to spaces, and init and free functionality added.

--- circular_buffer.c
+++ circular_buffer.c
@@ -2,3 +2,32 @@
 #include <stdio.h>
 
 #include "circular_buffer.h"
+
+CircularBuffer* cb_init(size_t capacity) 
+{
+    CircularBuffer *cb = malloc(sizeof(CircularBuffer));
+    if (!cb) return NULL;
+    
+    cb->buffer = malloc(sizeof(int) * capacity);
+
+    if (!cb->buffer) 
+    {
+        free(cb);
+        return NULL;
+    }
+    
+    cb->capacity = capacity;
+    cb->head = 0;
+    cb->tail = 0;
+    cb->full = false;
+    return cb;
+}
+
+void cb_free(CircularBuffer *cb) 
+{
+    if (cb) 
+    {
+        free(cb->buffer);
+        free(cb);
+    }
+}
